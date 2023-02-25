a144b02c5056ea8d5129bf51af9b3f76612a3b63
radmehr <alii.radmehr98@gmail.com>
2023-02-25 13:57:53 +0330

    postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ReportEntity } from './reports/report.entity';
@@ -6,25 +6,38 @@ import { ReportsModule } from './reports/reports.module';
 import { UserEntity } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 import * as Joi from 'joi';
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
+      // envFilePath: `.env.${process.env.NODE_ENV}`,
       validationSchema: Joi.object({
         DB_NAME: Joi.string().required(),
         PORT: Joi.number().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_HOST: Joi.string().required(),
+        DB_LOGGING: Joi.boolean().required(),
+        DB_SYNCHRONIZATION: Joi.boolean().required(),
+        COOKIE_KEY: Joi.string().required(),
       }),
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
         return {
-          type: 'sqlite',
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          post: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
           database: config.get<string>('DB_NAME'),
           entities: [UserEntity, ReportEntity],
-          synchronize: true,
+          synchronize: config.get<boolean>('DB_SYNCHRONIZATION'),
+          logging: config.get<boolean>('DB_LOGGING'),
         };
       },
     }),
@@ -38,4 +51,13 @@ import * as Joi from 'joi';
     ReportsModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(
+      cookieSession({
+        keys: [this.configService.get('COOKIE_KEY')],
+      }),
+    );
+  }
+}
